066fbddf711854962a5cc432410e055dc4bf93dc
Caroline <131356373+caro-cmd@users.noreply.github.com>
2025-09-29 11:36:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ function MemoryCard( {value, isFlipped, onCardClick}){
           
         </div>
         <div className="memory-card-back">
-          <img src={`src/assets/${value}.png`} alt="" /></div>
+          <img src={`../assets/${value}.png`} alt={value} /></div>
       </div>
     </div>
   );
